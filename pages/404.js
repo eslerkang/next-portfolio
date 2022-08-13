53ee4c349ea3a03d1000562a04fc07086a0705d8
eslerkang <eslerkang@gmail.com>
2022-08-13 21:35:57 +0900

    feat: add 404 page

---
+++ pages/404.js
@@ -0,0 +1,11 @@
+import Layout from "../components/layout";
+
+export default function PageNotFound() {
+  return (
+    <Layout>
+      <div className=" min-h-screen w-full flex items-center justify-center">
+        <h1 className=" text-5xl">404 | 페이지가 없습니다 ㅠㅠ</h1>
+      </div>
+    </Layout>
+  );
+}
