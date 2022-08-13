7483f15d638cc72a5a2ac31bd9ce8b558b2c14f9
eslerkang <eslerkang@gmail.com>
2022-08-13 10:01:43 +0900

    feat: add Links to buttons except social buttons

--- components/footer.js
+++ components/footer.js
@@ -1,20 +1,25 @@
+import Link from "next/link";
+
 export default function Footer() {
   return (
     <>
       <footer className="text-gray-600 body-font">
         <div className="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
-          <a className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
-            <span className="ml-3 text-xl">고민을 발굴하는 개발자</span>
-          </a>
+          <Link href="/">
+            <a className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
+              <span className="ml-3 text-xl">고민을 발굴하는 개발자</span>
+            </a>
+          </Link>
+
           <p className="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">
             © 2022
             <a
-              href="https://twitter.com/knyttneve"
+              href="https://github.com/eslerkang"
               className="text-gray-600 ml-1"
               rel="noopener noreferrer"
               target="_blank"
             >
-              @knyttneve
+              @eslerkang
             </a>
           </p>
           <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
--- components/home/hero.js
+++ components/home/hero.js
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export default function Hero() {
   return (
     <>
@@ -17,9 +19,11 @@ export default function Hero() {
           아름다우냐?
         </p>
         <div className="flex justify-center">
-          <button className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
-            프로젝트 보러가기
-          </button>
+          <Link href="/projects">
+            <a className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+              프로젝트 보러가기
+            </a>
+          </Link>
         </div>
       </div>
     </>
